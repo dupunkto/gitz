ae64a232978ebc90270c09134854010ee1ba3d4f
Robin <robindev2019@outlook.com>
2024-08-21 22:24:59 +0200

    First deployment!

--- src/config.php
+++ src/config.php
@@ -1,6 +1,6 @@
 <?php
-// Contains some basic configuration for getting gitty
+// Contains some basic configuration for getting gitz
 // up-and-running.
 
-define('SCAN_PATH', "/Users/axcelott/git-backup");
+define('SCAN_PATH', "/home/robinb");
 define('NAMESPACES', ['axcelott', 'dupunkto', 'forks', 'meta']);
