4ad159d4115c718f951c8a781427d12d84aa853a
Robin <robindev2019@outlook.com>
2024-08-22 18:03:40 +0200

    Override `SCAN_PATH` in development

--- src/config.php
+++ src/config.php
@@ -2,5 +2,5 @@
 // Contains some basic configuration for getting gitz
 // up-and-running.
 
-define('SCAN_PATH', "/home/robinb");
+define('SCAN_PATH', getenv("SCAN_PATH", local_only: true) ?: "/home/robinb");
 define('NAMESPACES', ['axcelott', 'dupunkto', 'forks', 'meta']);
