8ca6bf0fa638439460651cc38b664a35602b5786
Robin <robindev2019@outlook.com>
2025-02-16 11:52:39 +0100

    Fancy diffs using diff2html library
    Progressive enhancement goes brrrr

--- gui/commit.php
+++ gui/commit.php
@@ -1,3 +1,18 @@
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/diff2html/bundles/css/diff2html.min.css" />
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/diff2html/bundles/js/diff2html-ui.min.js"></script>
+
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/github.min.css"
+  media="screen and (prefers-color-scheme: light)"
+/>
+
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/github-dark.min.css"
+  media="screen and (prefers-color-scheme: dark)"
+/>
+
 <div class="container commit-details">
   <?php $commit = \core\getCommit($repo, $hash) ?>
 
@@ -19,3 +34,34 @@
 
   <pre><code><?= htmlspecialchars($commit['diff']) ?></code></pre>
 </div>
+
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+    const originElement = document.querySelector("pre");
+    const targetElement = document.createElement("div");
+    const containerElement = document.querySelector(".commit-details");
+
+    // Fix to prevent overflow
+    targetElement.style.maxWidth = "100%";
+
+    // Insert target div for for rendering diff2html
+    const code = originElement.innerText;
+    containerElement.insertBefore(targetElement, originElement);
+
+    // Remove original element from page
+    originElement.remove();
+    delete originElement;
+
+    const diff = new Diff2HtmlUI(targetElement, code, {
+      synchronisedScroll: true,
+      highlight: true,
+      drawFileList: false,
+      fileContentToggle: false,
+      stickyFileHeaders: false,
+      outputFormat: 'side-by-side',
+      colorScheme: 'auto'
+    });
+
+    diff.draw();
+  });
+</script>
