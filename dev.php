6f72ed4a52a2feae5376d69ac52f7320b7a95ab3
Robin <robindev2019@outlook.com>
2025-02-22 17:23:29 +0100

    Show errors in development

--- dev.php
+++ dev.php
@@ -5,6 +5,10 @@
 require_once __DIR__ . "/src/core.php";
 require_once __DIR__ . '/vendor/autoload.php';
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 $requested_file = path_join(__DIR__, $path);
 
 switch(true) {
