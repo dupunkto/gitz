8f47fc0c855191aeabaecaf3ba72df80648e9c12
Robin <robindev2019@outlook.com>
2025-02-22 17:26:55 +0100

    Disable error logging in prod environments

--- src/config.php
+++ src/config.php
@@ -8,3 +8,8 @@ define('MAX_REPOS', 7);
 
 define('SCAN_PATH', getenv("SCAN_PATH", local_only: true) ?: "/home/robinb");
 define('NAMESPACES', ['axcelott', 'dupunkto', 'forks', 'neopub', 'nindo', 'grape-lang', 'unlibrary', 'skylight',  'sites', 'legacy', 'meta']);
+
+// Disable error logging in prod environments
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+error_reporting(0);
