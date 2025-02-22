dfa9cc2e1b3492791bd667067711588a6b4b22a7
Robin <robindev2019@outlook.com>
2025-02-22 20:37:53 +0100

    Retain navigation context in tree view

--- gui/tree.php
+++ gui/tree.php
@@ -3,7 +3,7 @@
     <?php if(!in_array($request_path, ["", "."])): ?>
       <li>
         <code class="mode">d---------</code>
-        <a href="<?= $repo_url ?>/tree/<?= $hash ?>/<?= path_parent($request_path) ?>">
+        <a href="<?= $repo_url ?>/tree/<?= $params[1] ?>/<?= path_parent($request_path) ?>">
          ..
         </a>
       </li>
@@ -11,7 +11,7 @@
     <?php foreach(\core\getTree($repo, $request_path, $hash) as $child): ?>    
       <li>
         <code class="mode"><?= \core\fmtMode($child['mode']) ?></code>
-        <a href="<?= $repo_url ?>/<?= $child['type'] ?>/<?= $hash ?>/<?= path_join($request_path, $child['path']) ?>">
+        <a href="<?= $repo_url ?>/<?= $child['type'] ?>/<?= $params[1] ?>/<?= path_join($request_path, $child['path']) ?>">
           <?= $child['path'] ?>
         </a>
       </li>
