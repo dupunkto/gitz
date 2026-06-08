c3f5c7141dcc06bf50511900d6eb6ced3c3dcd42
Robin <robindev2019@outlook.com>
2026-06-08 14:14:06 +0200

    Fix HTML entities

--- partials/listing.php
+++ partials/listing.php
@@ -62,7 +62,7 @@
                 <time class="dt"><?= \dates\timeAgo($details['updated']) ?></time>
               <?php endif; ?>
               <h3><?= $details['name'] ?></h3>
-              <p><?= esc_inner(\core\getDescription($repo)) ?></p>
+              <p><?= $description ?></p>
             </a>
           <?php if($count <= $max_repos) echo "</li>" ?>
         <?php endforeach; ?>
