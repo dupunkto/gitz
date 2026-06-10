de66df33c36da6a25dfe40feb5e36c341aa73bd8
Robin <robindev2019@outlook.com>
2026-06-10 19:30:25 +0200

    Remove scttr namespace

--- core/config.php
+++ core/config.php
@@ -8,7 +8,7 @@ define('MAX_REPOS', 7);
 define('UNLISTED', true);
 
 define('SCAN_PATH', getenv("SCAN_PATH", local_only: true) ?: '/home/' . USER);
-define('NAMESPACES', ['axcelott', 'dupunkto', 'sites', 'meta', 'scttr', 'neopub', 'grape-lang', 'nindo', 'skylight', 'unlibrary', 'legacy', 'forks']);
+define('NAMESPACES', ['axcelott', 'dupunkto', 'sites', 'meta', 'neopub', 'grape-lang', 'nindo', 'skylight', 'unlibrary', 'legacy', 'forks']);
 define('LEGACY', ['neopub', 'grape-lang', 'nindo','skylight', 'unlibrary', 'legacy']);
 
 // Disable error logging in prod environments
