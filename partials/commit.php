45a41cd4dae8016fad95a74a62553e66f9eb1f2d
Robin <robindev2019@outlook.com>
2026-07-07 13:27:49 +0200

    Display full author in commit messages

--- partials/commit.php
+++ partials/commit.php
@@ -21,7 +21,7 @@
 
     <p>
       Commited on <time><?= \dates\humanReadable($commit['datetime']) ?></time>
-      by <a href="mailto:<?= esc_attr($commit['email']) ?>"><?= esc_inner($commit['author']) ?></a>. <a class="browse" href="<?= $repo_url ?>/tree/<?= $hash ?>">Browse files in this commit</a>
+      by <a href="mailto:<?= esc_attr($commit['email']) ?>"><?= esc_inner($commit['author']) ?> &lt;<?= esc_inner($commit['email']) ?>&gt;</a>. <a class="browse" href="<?= $repo_url ?>/tree/<?= $hash ?>">Browse files in this commit</a>
     </p>
   </hgroup>
 
